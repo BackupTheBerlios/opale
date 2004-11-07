39e33aa40ca70b951efb625da079156254187ffb
pac <pac>
2004-11-07 11:09:04 +0000

    added methods to manage the camera orientation and position allowing the user to turn around, move away or closer to the object

--- AGECYMO/src/camera.cpp
+++ AGECYMO/src/camera.cpp
@@ -3,15 +3,208 @@
 using namespace gml;
   
 Camera::Camera()
-{
-  _position[0] = 0; _position[1] = 0; _position[2] = 20;
+    : _thetaIncr(DEFAULT_INCR_THETA),
+      _phiIncr(DEFAULT_INCR_PHI),
+      _rhoIncr(DEFAULT_INCR_DIST),
+      _theta(M_PI/2.0),
+      _phi(0.0),
+      _rho(2.0)
+
+{  
+  //matrices to manage the camera
+  _matRotIncrY = Matrix3D::rotationY(   _phiIncr);
+  _matRotDecrY = Matrix3D::rotationY( - _phiIncr);
+
+  
+  _matRotIncrX = Matrix3D::rotationX(   _thetaIncr);
+  _matRotDecrX = Matrix3D::rotationX( - _thetaIncr);
+    
+  updateThetaTransformMatrix();
+  
+  
+  //camera parameters according to phi, theta and rho 
+  _position[0] = 0; _position[1] = 0; _position[2] = 2;
 
   _lookat[0] = 0; _lookat[1] = 0; _lookat[2] = 0;
 
   _up[0] = 0; _up[1] = 1; _up[2] = 0;
 
-  _fovy = 60;
+  _fovy = 120;
 }
+
+
+void
+Camera::incrementPhi()
+{
+  Point3D newPosition;
+
+  _phi +=  _phiIncr;
+  
+  newPosition = _matRotIncrY * _position;
+
+  _position = newPosition;
+  
+  updateThetaTransformMatrix();
+}
+
+void
+Camera::decrementPhi()
+{
+  Point3D newPosition;
+
+  _phi -=  _phiIncr;
+  
+  newPosition = _matRotDecrY * _position;
+
+  _position = newPosition;
+
+  updateThetaTransformMatrix();
+}
+
+    
+void
+Camera::incrementTheta()
+{
+  Point3D newPosition;
+
+  _theta += _thetaIncr;
+    
+  newPosition = _matIncrTheta * _position;
+  
+  _position = newPosition;
+    
+}
+
+void
+Camera::decrementTheta()
+{
+  Point3D newPosition;
+
+  _theta -= _thetaIncr;
+ 
+  newPosition = _matDecrTheta * _position;
+  
+  _position = newPosition;
+}
+
+void
+Camera::incrementDistance()
+{
+  std::cout << "increment Distance" << std::endl;
+  
+  Vector3D tr;
+
+  tr[0] = _position[0];
+  tr[1] = _position[1];
+  tr[2] = _position[2];
+  
+  tr.normalize();
+
+  tr = tr * _rhoIncr;
+    
+  _position[0] = _position[0] + tr[0];
+  _position[1] = _position[1] + tr[1];
+  _position[2] = _position[2] + tr[2];
+
+  _rho +=  _rhoIncr;
+}
+
+void
+Camera::decrementDistance()
+{
+  std::cout << "DECREMENT Distance" << std::endl;
+  
+  Vector3D tr;
+
+  tr[0] = _position[0];
+  tr[1] = _position[1];
+  tr[2] = _position[2];
+  
+  tr.normalize();
+
+  tr = tr *  (- _rhoIncr);
+    
+  _position[0] = _position[0] + tr[0];
+  _position[1] = _position[1] + tr[1];
+  _position[2] = _position[2] + tr[2];
+
+  _rho -=  _rhoIncr;
+}
+
+void
+Camera::setCameraPosition(gml::Point3D const& new_position)
+{
+  _position = new_position;
+  updateSphericalCoordinates();
+}
+
+
+void
+Camera::setCameraPosition(double px, double py, double pz)
+{
+  _position[0] = px;
+  _position[1] = py;
+  _position[2] = pz;
+
+  updateSphericalCoordinates();
+}
+
+void
+Camera::setCameraUp(double upx, double upy, double upz)
+{
+  _up[0] = upx;
+  _up[1] = upy;
+  _up[2] = upz;
+
+  updateAxis();
+}
+
+void
+Camera::setCameraLookAt(double cx, double cy, double cz)
+{
+  _lookat[0] = cx;
+  _lookat[1] = cy;
+  _lookat[2] = cz;
+
+  updateAxis();
+}
+
+void
+Camera::updateThetaTransformMatrix()
+{
+
+  _matIncrTheta = Matrix3D::rotationY( - _phi);
+  _matIncrTheta = _matRotIncrX *  _matIncrTheta;
+  _matIncrTheta =  Matrix3D::rotationY( + _phi) *  _matIncrTheta;
+  
+  _matDecrTheta = Matrix3D::rotationY( - _phi);
+  _matDecrTheta = _matRotDecrX *  _matDecrTheta;
+  _matDecrTheta = Matrix3D::rotationY( + _phi) *  _matDecrTheta;
+  
+}
+
+void
+Camera::updateSphericalCoordinates()
+{
+  double x2y2 =  _position[0] * _position[0] +_position[1] * _position[1];
+  
+  _rho = sqrt ( x2y2 + _position[2] * _position[2]);
+
+  _phi =  M_PI + atan2( _position[1], _position[0] ) ;
+
+  _theta =   M_PI/2.0 + atan( sqrt(x2y2) / _position[2] );
+}
+
+void
+Camera::updateCartesianCoordinates()
+{
+  double sint =  sin(_theta);
+    
+  _position[0] = _rho * cos(_phi) * sint;
+  _position[1] = _rho * sin(_phi) * sint ;
+  _position[2] = _rho * cos(_theta);
+}
+
 
 void
 Camera::updateAxis()
@@ -24,6 +217,4 @@ Camera::updateAxis()
   
   _y = cross(_x, _z);
   _y.normalize();
-
 }
-
--- AGECYMO/src/camera.hpp
+++ AGECYMO/src/camera.hpp
@@ -3,12 +3,26 @@
 
 #include "point.hpp"
 #include "vector.hpp"
+#include "matrix.hpp"
+
+
 
 namespace gml
 {
+
+  const  double DEFAULT_INCR_THETA = M_PI / 180.0;     //1 degree
+  const  double DEFAULT_INCR_PHI   = 5 * M_PI / 180.0; //5 degrees
+
+  const  double DEFAULT_INCR_DIST = 0.20;
   
+  
+  /**
+   * This is a simple camera which by default looks to the world origin
+   * Its moves are constrains to maintain its lookat point.
+   */
   class Camera
   {
+
     public:
     
     Point3D _position; //position of the camera in world coordinates
@@ -23,13 +37,58 @@ namespace gml
     Vector3D _y;
     Vector3D _z;
 
+    double _theta; // spherical coordinates
+    double _phi;   
+    double _rho;
+
+    double _thetaIncr;
+    double _phiIncr;
+    double _rhoIncr;
+    
+    
+    //Matrices to manage the camera moves
+    Matrix3D _matRotIncrY; //matric to turn around Y axis 
+    Matrix3D _matRotDecrY;
+    
+    Matrix3D _matRotIncrX; //matric to turn around X axis 
+    Matrix3D _matRotDecrX;
+    
+    Matrix3D _matIncrTheta;
+    Matrix3D _matDecrTheta;
+    
     public:
 
     Camera();
+
+    //Sets the position this will update spherical coordinates
+    void setCameraPosition(Point3D const& new_position);
+    void setCameraPosition(double px, double py, double pz);
+  
+    void setCameraUp(double upx, double upy, double upz);
+    void setCameraLookAt(double cx, double cy, double cz);
+
+    
+    
+    void incrementPhi();
+    void decrementPhi();
+    
+    void incrementTheta();
+    void decrementTheta();
+
+    void incrementDistance();
+    void decrementDistance();
+    
+
+    private:
+
+    void updateThetaTransformMatrix();
+
+    void updateSphericalCoordinates();
+    void updateCartesianCoordinates();
+    
     
     void updateAxis();
-
-        
+    
   };
 }
 
