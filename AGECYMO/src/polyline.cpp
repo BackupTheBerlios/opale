7ae03416ff20199fd8d599b6b4a51fe9d494d3b1
bigalex <bigalex>
2004-11-15 20:20:51 +0000

    *** empty log message ***

--- AGECYMO/src/polyline.cpp
+++ AGECYMO/src/polyline.cpp
@@ -1,100 +1,19 @@
 #include "polyline.hpp"
 
-Polyline::Polyline(){
-  _pointsVector.clear();
-  _isClosed = false;
-  _redComponent = 0.0;
-  _greenComponent = 0.0;
-  _blueComponent = 1.0;
-  _isSelected.clear();
-}
+Polyline::Polyline()
+  :
+  AbsCurve()
+{}
 
-Polyline::Polyline(std::vector <gml::Point3D> pointsVector, bool isClosed){
-  _pointsVector.clear();
-  for(unsigned i=0; i<pointsVector.size(); i++){
-    _pointsVector[i] = pointsVector[i];
-  }
-  _isClosed = isClosed;
+Polyline::Polyline(std::vector<gml::Point3D> pointsVector, bool isClosed)
+  :
+  AbsCurve(pointsVector, isClosed)
+{}
 
-  _redComponent = 0.0;
-  _greenComponent = 0.0;
-  _blueComponent = 1.0;
-  _isSelected.clear();
-}
-
-Polyline::Polyline(const Polyline &source){
-  for(int i = 0; i<source.getNbPoints(); i++){
-    _pointsVector[i] = source.getPoint(i);
-  }
-  _isClosed = source.isClosed();
-  _isSelected = source.isSelected();
-  
-}
-
-void Polyline::addPoint(gml::Point3D newPoint){
-  if(!_isClosed){
-    if(isExistingPoint(newPoint) == 0){
-      _isClosed = true;
-    }
-    else{
-      _pointsVector.push_back(newPoint);
-    }
-  }
-}
-
-void Polyline::addPointAtIndex(gml::Point3D newPoint, int index){
-  std::vector<gml::Point3D>::iterator it;
-  if(((unsigned)index <= _pointsVector.size()) || (index >= 0)){
-    it = _pointsVector.begin();
-    it += index;
-    _pointsVector.insert(it,newPoint);
-  }
-}
-
-int Polyline::getNbPoints() const{
-  return _pointsVector.size();
-}
-
-gml::Point3D Polyline::getPoint(int index) const{
-  if(((unsigned)index < _pointsVector.size()) || (index >=0)){
-    return _pointsVector[index];
-  }
-  exit(EXIT_FAILURE);
-}
-
-bool Polyline::isClosed() const{
-  return _isClosed;
-}
-
-void Polyline::movePoint(int index, gml::Point3D newPosition){
-  if(((unsigned)index<_pointsVector.size()) || (index >= 0)){ 
-    _pointsVector[index] = newPosition;
-  }
-}
-
-int Polyline::isExistingPoint(gml::Point3D point) const{
-  for(unsigned i = 0; i<_pointsVector.size(); i++){
-    if(((_pointsVector[i])[0] == point[0]) ||
-       ((_pointsVector[i])[1] == point[1])){
-      return i;
-    }
-  }
-  return NO_EXIST;
-}
-
-void Polyline::deletePoint(int index){
-  std::vector<gml::Point3D>::iterator it;
-  if(((unsigned)index<_pointsVector.size()) || (index >= 0)){ 
-    it =_pointsVector.begin();
-    it += index;
-    _pointsVector.erase(it);
-  }
-}
-
-void Polyline::setColor(double red, double green, double blue){
-  _redComponent = red;
-  _greenComponent = green;
-  _blueComponent = blue;
+Polyline::Polyline(const Polyline &source)
+  :
+  AbsCurve(source)
+{
 }
 
 void Polyline::render(){
@@ -116,8 +35,25 @@ void Polyline::render(){
 
   glEnd();
 
+  glColor3f(_redComponent, _greenComponent, _blueComponent);
+  bool selected;
   //we draw the control points
   for(int i = 0 ; i < int(_pointsVector.size()); i++){
+    
+    selected = false;
+    for(int j = 0; j<int(_isSelected.size()); j++){
+      if(i == _isSelected[j]){
+	std::cout<<"est selectionne : "<<_isSelected[j]<<std::endl;
+	selected = true;
+      }
+    }
+
+    if(selected){
+      glColor3f(1.0, 0.0, 0.0);
+    }
+    else{
+      glColor3f(_redComponent, _greenComponent, _blueComponent);
+    }
     glBegin(GL_POLYGON);
     glVertex2f(_pointsVector[i][0]-increment, _pointsVector[i][1]-increment);
     glVertex2f(_pointsVector[i][0]-increment, _pointsVector[i][1]+increment);
@@ -125,60 +61,12 @@ void Polyline::render(){
     glVertex2f(_pointsVector[i][0]+increment, _pointsVector[i][1]-increment);
     glEnd();
   }
-
 }
 
+std::vector<gml::Point3D> Polyline::discretize()
+{
+  std::vector<gml::Point3D> pointsList;
+  pointsList.clear();
 
-void Polyline::selection(double xUpLeft, 
-			 double yUpLeft,
-			 double xDownRight, 
-			 double yDownRight){
-  _isSelected.clear();
-
-  //we define the selected points
-  for(unsigned int i = 0; i<_pointsVector.size(); i++){
-    if((_pointsVector[i][0] >= xUpLeft ) || 
-       (_pointsVector[i][0] <= xDownRight ) ||
-       (_pointsVector[i][1] <= yUpLeft ) || 
-       (_pointsVector[i][1] >= yDownRight )){
-      
-      //we select the point
-      _isSelected.push_back(i);
-      
-    }
-  }
-}
-
-void Polyline::noSelection(){
-  _isSelected.clear();
-}
-
-std::vector<unsigned short> Polyline::isSelected() const{
-  return _isSelected;
-}
-
-void Polyline::select(unsigned short index){
-  if(index<_pointsVector.size()){ 
-    _isSelected.push_back(index);
-  }
-}
-
-double Polyline::getRed() const{
-  return _redComponent;
-}
-
-double Polyline::getGreen() const{
-  return _greenComponent;
-}
-
-double Polyline::getBlue() const{
-  return _blueComponent;
-}
-
-void Polyline::close(){
-  _isClosed = true;
-}
-
-void Polyline::open(){
-  _isClosed = false;
+  return pointsList;
 }
--- AGECYMO/src/polyline.hpp
+++ AGECYMO/src/polyline.hpp
@@ -4,61 +4,18 @@
 #include <vector>
 #include "point.hpp"
 #include "qgl.h"
+#include "abscurve.hpp"
 
-#define NO_EXIST -1;
-#define NO_SELECTION -1;
-const double Control_point_size = 0.10;
-
-class Polyline
+class Polyline : public AbsCurve 
 {
 private:
-  std::vector <gml::Point3D> _pointsVector;
-  bool _isClosed;
-  double _redComponent, _greenComponent, _blueComponent;
-  std::vector<unsigned short> _isSelected;
-
+  
 public:
   Polyline();
   Polyline(std::vector<gml::Point3D> pointsVector, bool isClosed);
   Polyline(const Polyline &source);
-
-  //add a point to the polyline
-  void addPoint(gml::Point3D newPoint);
-  //add a point at index
-  void addPointAtIndex(gml::Point3D newPoint, int index);
-  //return the number of points in the polyline
-  int getNbPoints() const;
-  //return the point at index
-  gml::Point3D getPoint(int index) const;
-  //true if the polyline is closed
-  bool isClosed() const;
-  //close the polyline
-  void close();
-  //open the polyline
-  void open();
-  //move the index point at newPosition
-  void movePoint(int index, gml::Point3D newPosition);
-  //return the index of the point if defined, -1 else
-  int isExistingPoint(gml::Point3D point) const;
-  //delete the index point
-  void deletePoint(int index);
-  //define if a point is selected
-  std::vector<unsigned short> isSelected() const;
-  //selection of a point
-  void select(unsigned short index);
-  //selection of few points
-  void selection(double xUpLeft, double yUpLeft,
-		 double xDownRight, double yDownRight);
-  void noSelection();
-  //define the color of the polyline
-  void setColor(double red, double green, double blue);
-  //draw the polyline
   void render();
-  //get the color values
-  double getRed() const;
-  double getGreen() const;
-  double getBlue() const;
-  
+  std::vector<gml::Point3D> discretize();
 };
 
 #endif
