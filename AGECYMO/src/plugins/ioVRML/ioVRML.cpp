ffe7649c440b7459c2b088efb01684fb7c7bd839
yakata <yakata>
2004-11-19 14:56:01 +0000

    modification du load

--- AGECYMO/src/plugins/ioVRML/ioVRML.cpp
+++ AGECYMO/src/plugins/ioVRML/ioVRML.cpp
@@ -89,24 +89,283 @@ void** query(void){
   return parameters;
 }
 
-// Sequence for points coordinates 
+/*// Sequence for points coordinates 
 static void sequence1Construction(vector<string> & sequence) {
-  sequence.push_back("point");
-  sequence.push_back("[");
-  sequence.push_back("]");
+  sequence.push_back(POINT_PARAMETER);
+  sequence.push_back(L_SBRACKET);
+  sequence.push_back(R_SBRACKET);
 }
 
 
 // Sequence for indexes (points order)
 static void sequence2Construction(vector<string> & sequence) {
-  sequence.push_back("coordIndex");
-  sequence.push_back("[");
-  sequence.push_back("]");
+  sequence.push_back(COORD_INDEX_PARAMETER);
+  sequence.push_back(L_SBRACKET);
+  sequence.push_back(R_SBRACKET);
+  }*/
+
+// In order to erase spaces
+static string eraseSpaces(string & text) {
+
+  string result(text);
+  string spaceString = " ";
+  string firstChar;
+  string lastChar;
+
+  //cout << "Dans eraseSpaces" << endl;
+  
+  // For the beginning
+  //cout << "result[0] :'" << result[0] << "'" << endl;
+  firstChar = result[0];
+  while (firstChar == spaceString) {
+    string strTemp(result, 1, result.length());
+    result = strTemp;
+    firstChar = result[0];
+    //cout << "resultBeg :" << result << endl;
+  }
+
+  // For the end
+  lastChar = result[result.length()-1];
+  while (lastChar == spaceString) {
+    string strTemp(result, 0, result.length()-1);
+    result = strTemp;
+    lastChar = result[result.length()-1];
+    //cout << "resultEnd :" << result << endl;
+  }
+
+  //cout << "Fin eraseSpaces" << endl;
+    
+  return result;
+}
+
+// Search words
+static vector<string> searchWords(string & text) {
+  
+  vector<string> listWords;
+  string newText;
+  string word;
+  string spaceString = " ";
+  string currentChar;
+
+  //cout << "Dans searchWords" << endl;
+
+  newText=eraseSpaces(text);
+  newText=newText + SPACE;
+  //cout << "newText :" << newText << endl;
+  for(unsigned int i=0; i<newText.length(); i++) {
+    //cout << "newText : " << newText[i] << endl;
+    
+    currentChar = newText[i];
+    if (currentChar == spaceString) {
+      if (!word.empty()) {
+	listWords.push_back(word);
+	//cout << "word : " << word << endl;
+	word = "";
+      }
+    }
+    else {
+      word = word + newText[i];
+    }
+  }
+
+  //cout << "Fin de searchWords" << endl;
+
+  return listWords;
 }
 
 extern "C"
 int load(MainWindow *mainW){
+
+  // Variable declaration
+  string allFile, word;
+  bool stillPoints = true, stillIndexes = true;
+  unsigned int placeToSearchPoints=0, placeToSearchIndexes=0, leftBracketPos=0, rightBracketPos=0;
+  double currentValue, xValue, yValue, zValue;
+  int numCoord = 1, nbPointsIndex = 0, indexesTranslation=0;
+
+  vector<int> indexValues;
+
+  int currentIndex;
+
+  vector<AbsFace*> *listFaces = new vector<AbsFace*>();
   
+
+  Tria *t;
+  Quad *q;
+  Face *f;
+
+  vector<Point3D> *listPoint = new vector<Point3D>();
+
+  // The user chooses a file in order to load a VRML model
+  QString fileName = QFileDialog::getOpenFileName(
+						  ".",
+						  "*.wrl",
+						  mainW,
+						  "VRML load dialog box",
+						  "choose a name for VRML load" );
+
+  // If there is no name defined exit
+  if(fileName.isEmpty()){
+    return EXIT_FAILURE;
+  }
+  
+  // Open the file
+  ifstream VRMLFile (fileName,ios::in);
+
+  // If the file doesn't exist
+  if(!VRMLFile){ 
+    cerr << "File not found !" << endl; 
+    return EXIT_FAILURE;
+  } 
+
+  // Reading of the file
+  while (!VRMLFile.eof()){ 
+    //cout << word << endl;
+    VRMLFile >> word;
+    allFile = allFile + word + SPACE;
+  }
+
+  // In order to receive the list of words
+  vector<string> listWords;
+
+  //cout << "Avant boucle" << endl;
+
+  // Search the points
+  while(stillPoints) {
+    
+    // Position of a point's occurence 
+    placeToSearchPoints = allFile.find(POINT_PARAMETER, rightBracketPos + strlen(R_SBRACKET));
+    
+    // If there is no points anymore
+    if (placeToSearchPoints == string::npos) {
+      stillPoints = false;
+    }
+    else {
+      
+      // Search of a left bracket
+      leftBracketPos = allFile.find(L_SBRACKET, placeToSearchPoints + strlen(POINT_PARAMETER));
+      rightBracketPos = allFile.find(R_SBRACKET, leftBracketPos + strlen(L_SBRACKET));
+      if (leftBracketPos != string::npos) {
+
+	string strTemp(allFile, leftBracketPos + strlen(L_SBRACKET), rightBracketPos-leftBracketPos-strlen(R_SBRACKET));
+	//cout << "strTemp : " << strTemp << endl;
+	listWords = searchWords(strTemp);
+	
+	// Reading of each words
+	for (unsigned int i=0; i<listWords.size(); i++) {
+	  currentValue = atof(listWords[i].c_str());
+	  //cout << "currentVal : " << currentValue << endl;
+
+	  // Coordinate X management
+	  if (numCoord == 1) {
+	    xValue = currentValue;
+	    numCoord++;
+	  }
+	  else {
+	    // Coordinate Y management
+	    if (numCoord == 2) {
+	      yValue = currentValue;
+	      numCoord++;
+	    }
+	    // Coordinate Z management 
+	    else {
+	      zValue = currentValue;
+	      Point3D p;
+	      p[0] = xValue;
+	      p[1] = yValue;
+	      p[2] = zValue;
+	      cout << "x=" << p[0] << "y=" << p[1] << "z=" << p[2] << endl;
+	      listPoint->push_back(p);
+	      numCoord = 1;
+	      
+	    }
+	  }
+	}
+      }
+    }
+  }
+
+	
+  while(stillIndexes) {
+    
+    // Position of a index's occurence 
+    placeToSearchIndexes = allFile.find(COORD_INDEX_PARAMETER, rightBracketPos + strlen(R_SBRACKET));
+    
+    // If there is no points anymore
+    if (placeToSearchIndexes == string::npos) {
+       stillIndexes = false;
+    }
+    else {
+      
+      // Search of a left bracket
+      leftBracketPos = allFile.find(L_SBRACKET, placeToSearchIndexes + strlen(COORD_INDEX_PARAMETER));
+      rightBracketPos = allFile.find(R_SBRACKET, leftBracketPos + strlen(L_SBRACKET));
+      
+      if (leftBracketPos != string::npos) {
+
+	string strTemp(allFile, leftBracketPos + strlen(L_SBRACKET), rightBracketPos-leftBracketPos-strlen(R_SBRACKET));
+	cout << "strTemp : " << strTemp << endl;
+	listWords = searchWords(strTemp);
+
+	// Management of the last -1
+	if (listWords.back() != "-1") {
+	  listWords.push_back("-1");
+	}
+	
+	// Reading of each words
+	for (unsigned int i=0; i<listWords.size(); i++) {
+	  
+	  currentIndex = atof(listWords[i].c_str());
+	  
+	  if (currentIndex == -1) {
+
+	    if (nbPointsIndex < 3) {
+	      return EXIT_FAILURE;
+	    }
+	    
+	    // Moving of indexes
+	    for (int i=0; i<=nbPointsIndex;i++) {
+	      indexValues[i] += indexesTranslation;
+	    }
+	    
+	    if (nbPointsIndex == 3) {
+	      t = new Tria(listPoint, indexValues[0], indexValues[1], indexValues[2]);
+	      listFaces->push_back(t);
+	    }
+	    else {
+	      if (nbPointsIndex == 4) {
+		q = new Quad(listPoint, indexValues[0], indexValues[1], indexValues[2], indexValues[3]);
+		listFaces->push_back(q);
+	      }
+	      else {
+		f = new Face(&indexValues, listPoint, nbPointsIndex);
+		listFaces->push_back(f);
+	      }
+	    }
+
+	    nbPointsIndex = 0;
+	    indexValues.clear();
+	    
+	  }
+	  else {
+	    indexValues.push_back(currentIndex);
+	    nbPointsIndex++;
+	  }
+	}
+      }
+    }
+
+    indexesTranslation = indexesTranslation + listPoint->size();
+      
+  }
+
+
+  Faces* faces = new Faces(listPoint, listFaces);
+  //cout << "La face :" << endl << *f;
+  mainW->setModel(*faces);
+      
+
+  /*
   // Using of the main window in order to enable the model access
   //MainWindow *mainW = (MainWindow*)mainWindow[0];
 
@@ -267,33 +526,31 @@ int load(MainWindow *mainW){
     if (awaitedSequence2.empty()) {
 
 
-      //nbPointsIndex--;
+      
       // Management of the last index
-      if (nbPointsIndex < 3) {
-	return EXIT_FAILURE;
-      }
+      if (nbPointsIndex > 3) {
       
-      // Moving of indexes
-      for (int i=0; i<=nbPointsIndex;i++) {
-	indexValues[i] += indexesTranslation;
-      }
-      
-      if (nbPointsIndex == 3) {
-	t = new Tria(listPoint, indexValues[0], indexValues[1], indexValues[2]);
-	listFaces->push_back(t);
-      }
-      else {
-	if (nbPointsIndex == 4) {
-	  q = new Quad(listPoint, indexValues[0], indexValues[1], indexValues[2], indexValues[3]);
-	  listFaces->push_back(q);
+	// Moving of indexes
+	for (int i=0; i<=nbPointsIndex;i++) {
+	  indexValues[i] += indexesTranslation;
+	}
+	
+	if (nbPointsIndex == 3) {
+	  t = new Tria(listPoint, indexValues[0], indexValues[1], indexValues[2]);
+	  listFaces->push_back(t);
 	}
 	else {
-	  f = new Face(&indexValues, listPoint, nbPointsIndex);
-	  listFaces->push_back(f);
+	  if (nbPointsIndex == 4) {
+	    q = new Quad(listPoint, indexValues[0], indexValues[1], indexValues[2], indexValues[3]);
+	    listFaces->push_back(q);
+	  }
+	  else {
+	    f = new Face(&indexValues, listPoint, nbPointsIndex);
+	    listFaces->push_back(f);
+	  }
 	}
+      
       }
-
-
       // Indexes management
       indexesTranslation = indexesTranslation + listPoint->size();
     
@@ -309,7 +566,7 @@ int load(MainWindow *mainW){
 
   qDebug("Fin de la methode LOAD du plugin VRML");
   
-
+  */
   return EXIT_SUCCESS;
 }
 
