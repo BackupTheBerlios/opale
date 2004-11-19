e21a5e5df740cb18e0126ca75cc146e64f98f16c
bigalex <bigalex>
2004-11-19 23:16:47 +0000

    events management

--- AGECYMO/src/polyline.hpp
+++ AGECYMO/src/polyline.hpp
@@ -2,7 +2,6 @@
 #define CLASS_POLYLINE
 
 #include <vector>
-#include "point.hpp"
 #include "qgl.h"
 #include "abscurve.hpp"
 
@@ -11,11 +10,17 @@ class Polyline : public AbsCurve
 private:
   
 public:
-  Polyline();
-  Polyline(std::vector<gml::Point3D> pointsVector, bool isClosed);
+  Polyline(Canvas2D *parent);
+  Polyline(std::vector<gml::Point3D> pointsVector, 
+	   bool isClosed,
+	   Canvas2D *parent);
   Polyline(const Polyline &source);
   void render();
+  void manageEvent(QMouseEvent* event,
+		   unsigned short toolType,
+		   unsigned short canvasType);
   std::vector<gml::Point3D> discretize();
+  void calculateQtToOpenGL(QMouseEvent* event, gml::Point3D *point);
 };
 
 #endif
