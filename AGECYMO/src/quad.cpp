8fe2115c3198ec75091ae4a9281b895bd1afa1fb
pac <pac>
2004-11-21 18:24:49 +0000

    added normal computing and display methods

--- AGECYMO/src/quad.cpp
+++ AGECYMO/src/quad.cpp
@@ -7,6 +7,7 @@ Quad::Quad(std::vector<gml::Point3D> *points, int i1, int i2, int i3, int i4)
       _i3(i3),
       _i4(i4)
 {
+  updateNormal();
 }
 
 void
@@ -18,6 +19,8 @@ Quad::render()
 
   
   glBegin(GL_QUADS);
+  
+  glNormal3f(_normal[0], _normal[1], _normal[2]);
 
   glColor3f(0.0, 1.0, 0.0);
   glVertex3d(tpoints[_i1][0],
@@ -38,6 +41,34 @@ Quad::render()
   
   glEnd();
 }
+
+void
+Quad::renderNormal()
+{
+
+  glBegin(GL_LINES);
+
+  glColor3f(1.0, 0.0, 1.0);
+  
+  glVertex3d(_center[0],
+             _center[1],
+             _center[2]);
+   
+  glVertex3d(_n1[0],
+             _n1[1],
+             _n1[2]);
+  
+  glEnd();
+}
+
+
+void
+Quad::renderWithNormal()
+{
+  render();
+  renderNormal();
+}
+
 
 std::vector<int>*
 Quad::getIndexes()
@@ -69,6 +100,49 @@ Quad::getIndexes()
 //   }
 // }
 
+
+void
+Quad::updateNormal()
+{  
+  std::vector<gml::Point3D> tpoints = *_points;
+
+  gml::Vector3D v1 = tpoints[_i2] -  tpoints[_i1];
+  gml::Vector3D v2 = tpoints[_i3] -  tpoints[_i1];
+  
+  gml::Vector3D v3 = tpoints[_i3] -  tpoints[_i2];
+  gml::Vector3D v4 = tpoints[_i4] -  tpoints[_i2];
+
+  gml::Vector3D normal1 = cross(v1, v2);
+  gml::Vector3D normal2 = cross(v3, v4);
+
+  //TODO: remove it ?
+  normal1.normalize();
+  normal2.normalize();
+  
+  _normal = (normal1 + normal2) * 0.5;
+
+  _normal.normalize();
+    
+  _center[0] =  (tpoints[_i1][0] +
+                 tpoints[_i2][0] +
+                 tpoints[_i3][0] +
+                 tpoints[_i4][0]) / 4.0;
+  
+  _center[1] =  (tpoints[_i1][1] +
+                 tpoints[_i2][1] +
+                 tpoints[_i3][1] +
+                 tpoints[_i4][1]) / 4.0;
+
+  _center[2] =  (tpoints[_i1][2] +
+                 tpoints[_i2][2] +
+                 tpoints[_i3][2] +
+                 tpoints[_i4][2]) / 4.0;
+  
+  _n1[0] = _normal[0] + _center[0];
+  _n1[1] = _normal[1] + _center[1];
+  _n1[2] = _normal[2] + _center[2];
+
+}
 
 
 
--- AGECYMO/src/quad.hpp
+++ AGECYMO/src/quad.hpp
@@ -13,17 +13,29 @@ class Quad : public AbsFace
   int _i3;
   int _i4;
 
+
+  //See updateNormal for computing details
+  //Quad is not necessarly a planar surface
+  gml::Vector3D _normal;
+  gml::Point3D _n1; //n1 - center = normalized normal of the triangle 
+  gml::Point3D _center;
+
   public:
   Quad(std::vector<gml::Point3D> *points,
        int i1 = 0, int i2 =1,
        int i3 = 2, int i4 = 3);
 
-  virtual void render();
+  void render();
+
+  void renderNormal();
+  
+  void renderWithNormal();
+
+  void updateNormal();
   
   virtual std::vector<int> *getIndexes();
 
-//   virtual int nbPoints();
-//   virtual int getIndex(int num);
+
   friend std::ostream& operator<<(std::ostream& os, Quad const& q);
 };
 
