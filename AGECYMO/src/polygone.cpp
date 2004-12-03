66b78884de5a4c6331b32c9e4098632ffc84c452
yakata <yakata>
2004-12-03 21:27:20 +0000

    polygone discretization

--- AGECYMO/src/polygone.cpp
+++ AGECYMO/src/polygone.cpp
@@ -15,7 +15,7 @@ Polygone::Polygone(Canvas2D *parent) {
   _parent = parent;
   
   int res = QInputDialog::getInteger("Another Modeler", "Number of points:", 3, 
-				     3, 360, 1,
+				     3, 12, 1,
 				     &ok, _parent);
   if ( ok ) {
     _nbPoints=res;
@@ -60,7 +60,7 @@ void Polygone::render(){
     float r = sqrt(x + y);
     
     glBegin(GL_LINE_STRIP);
-    for ( int i = 0 ; i < 360 ; i++ ) {
+    for ( int i = 0 ; i < 360 ; i++) {
       float angle = (float)i * 3.14159F / ((float)_nbPoints/2.0) ;
       float x = (float) ((*_pointsVector[0])[0] + r*cos(angle)) ;
       float y = (float) ((*_pointsVector[0])[1] + r*sin(angle)) ;
@@ -86,47 +86,79 @@ void Polygone::render(){
 std::vector<gml::Point3D> Polygone::discretize(int nbSegments)
 {
   std::vector<gml::Point3D> pointsList;
+  std::vector<gml::Point3D> pointsPoly;
 
-  /*// Ray compute
-  float xRay = pow((float)_pointsVector[1][0] - (float)_pointsVector[0][0],2);
-  float yRay = pow((float)_pointsVector[1][1] - (float)_pointsVector[0][1],2);
-  
-  //cout << "x = " << x << endl;
-  
-  float r = sqrt(xRay + yRay);
 
-  int step = 360/nbSegments;
-  
-  for ( int i = 0 ; i < 360 ; i=i+step ) {
-    float angle = i * 3.14159F / 180 ;
-    float x = (float) (_pointsVector[0][0] + r*cos(angle)) ;
-    float y = (float) (_pointsVector[0][1] + r*sin(angle)) ;
-    Point3D p;
-    p[0] = x;
-    p[1] = y;
-    p[2] = 0.0;
-    pointsList.push_back(p);
+  // Polygone
+  if (getNbPoints() == 2) {
+    
+    
+    // Ray compute
+    float x = pow((float)(*_pointsVector[1])[0] - (float)(*_pointsVector[0])[0],2);
+    float y = pow((float)(*_pointsVector[1])[1] - (float)(*_pointsVector[0])[1],2);
+    
+    float r = sqrt(x + y);
+    
+
+    for ( int i = 0 ; i < 360 ; i ++) {
+      float angle = (float)i * 3.14159F / ((float)_nbPoints/2.0) ;
+      float x = (float) ((*_pointsVector[0])[0] + r*cos(angle)) ;
+      float y = (float) ((*_pointsVector[0])[1] + r*sin(angle)) ;
+      Point3D p;
+      p[0] = x;
+      p[1] = y;
+      p[2] = 0.0;
+      pointsPoly.push_back(p) ;
+    }
+
+    for (int i=0; i<_nbPoints ; i++) {
+      pointsList.push_back(pointsPoly[i]);
+      float diffx = fabs(pointsPoly[i][0] - pointsPoly[i+1][0]);
+      float diffy = fabs(pointsPoly[i][1] - pointsPoly[i+1][1]);
+      int nbPointsSegment = nbSegments/_nbPoints;
+      for (int j=1; j<nbPointsSegment;j++) {
+	if (pointsPoly[i][0] < pointsPoly[i+1][0] && pointsPoly[i][1] < pointsPoly[i+1][1]) {
+	   Point3D p;
+	   p[0] = pointsPoly[i][0] + (diffx*(j/((float)nbSegments/(float)_nbPoints)));
+	   p[1] = pointsPoly[i][1] + (diffy*(j/((float)nbSegments/(float)_nbPoints)));
+	   p[2] = 0.0;
+	   pointsList.push_back(p);
+	}
+	else {
+	  if (pointsPoly[i][0] > pointsPoly[i+1][0] && pointsPoly[i][1] < pointsPoly[i+1][1]) {
+	    Point3D p;
+	    p[0] = pointsPoly[i][0] - (diffx*(j/(float)(nbSegments/(float)_nbPoints)));
+	    p[1] = pointsPoly[i][1] + (diffy*(j/(float)(nbSegments/(float)_nbPoints)));
+	    p[2] = 0.0;
+	    pointsList.push_back(p);
+	  }
+	  else {
+	    if (pointsPoly[i][0] > pointsPoly[i+1][0] && pointsPoly[i][1] > pointsPoly[i+1][1]) {
+	      Point3D p;
+	      p[0] = pointsPoly[i][0] - (diffx*(j/(float)(nbSegments/(float)_nbPoints)));
+	      p[1] = pointsPoly[i][1] - (diffy*(j/(float)(nbSegments/(float)_nbPoints)));
+	      p[2] = 0.0;
+	      pointsList.push_back(p);
+	    }
+	    else {
+	      Point3D p;
+	      p[0] = pointsPoly[i][0] + (diffx*(j/(float)(nbSegments/(float)_nbPoints)));
+	      p[1] = pointsPoly[i][1] - (diffy*(j/(float)(nbSegments/(float)_nbPoints)));
+	      p[2] = 0.0;
+	      pointsList.push_back(p);
+	    }
+	  }
+	}
+	//cout << "x= " << pointsList[i][0] << " y= " << pointsList[i][1] << endl;
+      }
+    }
+
   }
 
-  // In order to manage the last point of the revolution
-  float angle = 360 * 3.14159F / 180 ;
-  float xLastPoint = (float)(_pointsVector[0][0] + r*cos(angle)) ;
-  float yLastPoint = (float)(_pointsVector[0][1] + r*sin(angle)) ;
-  Point3D p;
-  p[0] = xLastPoint;
-  p[1] = yLastPoint;
-  p[2] = 0.0;
-  pointsList.push_back(p);
-
-  float angle = 0 * 3.14159F / 180 ;
-  float xLastPoint = (float)(_pointsVector[0][0] + r*cos(angle)) ;
-  float yLastPoint = (float)(_pointsVector[0][1] + r*sin(angle)) ;
-  Point3D p;
-  p[0] = xLastPoint;
-  p[1] = yLastPoint;
-  p[2] = 0.0;
-  pointsList.push_back(p);
-  */
+  /*for (int i=0; i<pointsList.size();i++) {
+    cout << "x= " << pointsList[i][0] << " y= " << pointsList[i][1] << endl;
+    }*/
+  
   return pointsList;
 }
 
