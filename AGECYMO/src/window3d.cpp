d0afb3b9718e93be61495c56b0eb48e6ad915586
bigalex <bigalex>
2004-12-05 19:01:13 +0000

    add javadoc comments

--- AGECYMO/src/window3d.cpp
+++ AGECYMO/src/window3d.cpp
@@ -5,30 +5,40 @@
 
 int Window3D::frame_cpt = 1;
 
+/**************************************************************
+ *
+ *  Window3D constructor
+ *  @param parent the parent canvas
+ *  @param name the name
+ *
+ *************************************************************/
 Window3D::Window3D(QWidget* parent, const char* name)
     : QFrame(parent, name),
       TITLE(QString("Perspective View ") + QString::number(frame_cpt++)),
     _layout(this, QBoxLayout::LeftToRight)//,
-//      _canvas(this)
-{
-//  _layout = new QBoxLayout(this, QBoxLayout::LeftToRight);
-
-//  _canvas = acanvas;
-  
+{ 
   setMidLineWidth(MID_LINE_WIDTH);
   setFrameShape(QFrame::Panel);
   setFrameShadow(QFrame::Plain);
   setCaption(TITLE);
-
-  //_layout.addWidget(_canvas);
-  
 }
 
+/**************************************************************
+ *
+ *  Window3D destructor
+ *
+ *************************************************************/
 Window3D::~Window3D()
 {
   delete _canvas;
 }
 
+/**************************************************************
+ *
+ *  attach a canvas to this
+ *  @param the abscanvas to attach
+ *
+ *************************************************************/
 void
 Window3D::attachCanvas(AbsCanvas* acanvas)
 {
@@ -36,25 +46,52 @@ Window3D::attachCanvas(AbsCanvas* acanvas)
   _layout.addWidget(_canvas);
 }
 
+/**************************************************************
+ *
+ *  get the attached AbsCanvas
+ *  @return the attached AbsCanvas
+ *
+ *************************************************************/
 AbsCanvas&
 Window3D::canvas()
 {
   return *(_canvas);
 }
 
-
+/**************************************************************
+ *
+ *  set the position of the camera
+ *  @param newPosition a vector for camera position
+ *
+ *************************************************************/
 void
 Window3D::setCameraPosition(gml::Point3D const& new_position)
 {
   // _canvas->setCameraPosition(new_position);
 }
 
+/**************************************************************
+ *
+ *  set the camera position
+ *  @param px x coordinate of camera
+ *  @param py y coordinate of camera
+ *  @param pz z coordinate of camera
+ *
+ *************************************************************/
 void
 Window3D::setCameraPosition(double px, double py, double pz)
 {
   //_canvas->setCameraPosition(px, py, pz);
 }
 
+/**************************************************************
+ *
+ *  define the up vector of the camera
+ *  @param px x coordinate of camera
+ *  @param py y coordinate of camera
+ *  @param pz z coordinate of camera
+ *
+ *************************************************************/
 void
 Window3D::setCameraUp(double upx, double upy, double upz)
 {
--- AGECYMO/src/window3d.hpp
+++ AGECYMO/src/window3d.hpp
@@ -7,6 +7,10 @@
 #include "abscanvas.hpp"
 #include "canvas3d.hpp"
 
+/******************************************************
+ *  derived from QFrame. openGL 3D window
+ *
+ ******************************************************/
 class Window3D : public QFrame
 {
   private:
@@ -21,19 +25,66 @@ class Window3D : public QFrame
   AbsCanvas* _canvas;
     
   public:
+
+  /**************************************************************
+   *
+   *  Window3D constructor
+   *  @param parent the parent canvas
+   *  @param name the name
+   *
+   *************************************************************/
   Window3D(QWidget* parent = 0,
            const char* name= 0);
 
+  /**************************************************************
+   *
+   *  Window3D destructor
+   *
+   *************************************************************/
   virtual ~Window3D();
 
+  /**************************************************************
+   *
+   *  attach a canvas to this
+   *  @param the abscanvas to attach
+   *
+   *************************************************************/
   void attachCanvas(AbsCanvas *abscanvas);
 
+  /**************************************************************
+   *
+   *  get the attached AbsCanvas
+   *  @return the attached AbsCanvas
+   *
+   *************************************************************/
   AbsCanvas& canvas();
   
+  /**************************************************************
+   *
+   *  set the position of the camera
+   *  @param newPosition a vector for camera position
+   *
+   *************************************************************/
   void setCameraPosition(gml::Point3D const& new_position);
 
+  /**************************************************************
+   *
+   *  set the camera position
+   *  @param px x coordinate of camera
+   *  @param py y coordinate of camera
+   *  @param pz z coordinate of camera
+   *
+   *************************************************************/
   void setCameraPosition(double px, double py, double pz);
 
+  /**************************************************************
+   *
+   *  define the up vector of the camera
+   *  @param px x coordinate of camera
+   *  @param py y coordinate of camera
+   *  @param pz z coordinate of camera
+   *
+   *************************************************************/
   void setCameraUp(double upx, double upy, double upz);
 
   
