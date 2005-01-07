1adf9671d2e5b1d63ff69ad17db0cc384c9d2174
pac <pac>
2005-01-07 15:40:47 +0000

    Initial import of the CORE library

---
+++ AGECYMO/lib/core/CORE.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+ * Core Library Version 1.7, August 2004
+ * Copyright (c) 1995-2004 Exact Computation Project
+ * All rights reserved.
+ *
+ * This file is part of CORE (http://cs.nyu.edu/exact/core/); you may
+ * redistribute it under the terms of the Q Public License version 1.0.
+ * See the file LICENSE.QPL distributed with CORE.
+ *
+ * Licensees holding a valid commercial license may use this file in
+ * accordance with the commercial license agreement provided with the
+ * software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * File: CORE.h
+ * Synopsis:
+ *      The main inclusion file for the Core Library system.
+ *      All "Core programs" must include this file.
+ * Note:
+ * 	Since version 1.6, we have moved all header files to inc/CORE,
+ * 	for backward compatibility, we keep this file.
+ *
+ * Written by 
+ *       Chee Yap <yap@cs.nyu.edu>
+ *       Chen Li <chenli@cs.nyu.edu>
+ *       Zilin Du <zilin@cs.nyu.edu>
+ *
+ * WWW URL: http://cs.nyu.edu/exact/
+ * Email: exact@cs.nyu.edu
+ *
+ * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/opale/Repository/AGECYMO/lib/core/CORE.h,v $
+ * $Revision: 1.1 $ $Date: 2005/01/07 15:40:47 $
+ ***************************************************************************/
+
+#include <CORE/CORE.h>
+
