634384dfbb8a63a340eca9bf85eb4a25f4e15821
pac <pac>
2005-01-11 20:54:12 +0000

    gml namespace removed

--- AGECYMO/src/precision.cpp
+++ AGECYMO/src/precision.cpp
@@ -1,32 +1,27 @@
 #include "precision.hpp"
 
 
-namespace gml
-{
-  //Static variable initialization
-  bool Precision::instanceFlag = false;
 
-  Precision* Precision::single = NULL;
+//Static variable initialization
+bool Precision::instanceFlag = false;
+
+Precision* Precision::single = NULL;
 
   
 //Static Methods
   
 //Method to retrieve an instance of Precision
-  Precision* Precision::getInstance()
+Precision* Precision::getInstance()
+{
+  if(! instanceFlag)
   {
-    if(! instanceFlag)
-    {
-      single = new Precision();
-
+    single = new Precision();
+    
       instanceFlag = true;
       return single;
-    }
-    else
-    {
-      return single;
-    }
   }
-  
-  
- 
+  else
+  {
+    return single;
+  }
 }
